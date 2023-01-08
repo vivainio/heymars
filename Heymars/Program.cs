b327b79ad4c7c99ecabbeabc5f9fdcf855edf73d
Ville M. Vainio <ville.vainio@basware.com>
2023-01-08 17:40:38 +0200

    add descriptins to commands

--- Heymars/Program.cs
+++ Heymars/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Spectre.Console.Cli;
 using System;
+using System.ComponentModel;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -7,10 +8,13 @@ namespace GuiLaunch
 {
     public class LauncherSettings: CommandSettings
     {
-        [CommandArgument(0, "[ConfigFile]")]
+        [Description("Path to config file. Can be .json, .jsonnet or flat text file")]
+        [CommandArgument(0, "<CONFIG_FILE>")]
         public string Config { get; set; }
+        [Description("Run without GUI. This automatically launches all the commands at the same time")]
         [CommandOption("--headless")]
         public bool Headless { get; set; }
+        [Description("When running with gui mode, launch all commands at the same time")]
         [CommandOption("--runall")]
         public bool RunAll { get; set; }
 
@@ -23,7 +27,7 @@ namespace GuiLaunch
         public override async Task<int> ExecuteAsync(CommandContext context, LauncherSettings settings)
         {
             var eng = new GuiLaunchEngine();
-            await eng.PopulateFromConfigFile(settings.Config ?? "commands.txt");
+            await eng.PopulateFromConfigFile(settings.Config);
             if (settings.Headless)
             {
                 await eng.RunAll();
